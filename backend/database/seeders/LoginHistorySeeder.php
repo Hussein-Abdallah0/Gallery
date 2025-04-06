6daec691edb1c41363f99bd3551c86678a5c2187
Hussein-02 <hussein02ab@gmail.com>
2025-04-06 14:52:43 +0300

    Finished login history factory and seeders

--- backend/database/seeders/LoginHistorySeeder.php
+++ backend/database/seeders/LoginHistorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\LoginHistory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class LoginHistorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        LoginHistory::factory(10)->create();
     }
 }
