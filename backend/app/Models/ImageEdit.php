cf8ab951da3929530cf8f9dfeb9ea25f0006d7d0
Hussein-02 <hussein02ab@gmail.com>
2025-04-06 14:58:46 +0300

    Finished image edit model

--- backend/app/Models/ImageEdit.php
+++ backend/app/Models/ImageEdit.php
@@ -9,4 +9,15 @@ class ImageEdit extends Model
 {
     /** @use HasFactory<\Database\Factories\ImageEditFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'image_id',
+        'operation_type',
+        'operation_details',
+    ];
+
+    public function userImage()
+    {
+        return $this->belongsTo(Image::class);
+    }
 }
