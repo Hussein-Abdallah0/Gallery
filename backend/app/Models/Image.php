d8839240fa184ca4a306f8a24eac85cb88062d29
Hussein-02 <hussein02ab@gmail.com>
2025-04-06 14:58:19 +0300

    Finished image model

--- backend/app/Models/Image.php
+++ backend/app/Models/Image.php
@@ -9,4 +9,20 @@ class Image extends Model
 {
     /** @use HasFactory<\Database\Factories\ImageFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'file_path',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function imageEdits()
+    {
+        return $this->hasMany(ImageEdit::class);
+    }
 }
