a0e353af1f267d13da008a8f9f8402b9dabab9b0
Hussein-02 <hussein02ab@gmail.com>
2025-04-08 12:45:24 +0300

    fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import WithoutSidebar from "./layouts/WithoutSidebar";
+import WithSidebar from "./layouts/WithSidebar";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Outlet />
-    </>
+    <Routes>
+      {/* Routes WITHOUT Sidebar */}
+      <Route element={<WithoutSidebar />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+
+      {/* Routes WITH Sidebar */}
+      <Route element={<WithSidebar />}>
+        <Route path="/home" element={<Home />} />
+        {/* Add more sidebar-wrapped routes here */}
+      </Route>
+    </Routes>
   );
 }
 
