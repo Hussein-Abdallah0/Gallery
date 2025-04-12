d557f3737ee6c4abadce85baf30e0cbf37947311
Hussein-02 <hussein02ab@gmail.com>
2025-04-12 19:09:29 +0300

    Added message request file and cleaned message controller

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateMessageRequest;
 use App\Models\Message;
-use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function store(Request $request)
+    public function store(CreateMessageRequest $request)
     {
-        $request->validate([
-            'sender_name' => 'required|string|max:255',
-            'content' => 'required|string'
-        ]);
-
         $message = Message::create($request->only(['sender_name', 'content']));
 
-        return response()->json($message, 201);
+        return $this->successResponse($message, 201);
     }
 
     public function index()
