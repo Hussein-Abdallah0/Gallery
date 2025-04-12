36220f0ee45d58a8c1cbc6f8767cf289484e15de
Hussein-02 <hussein02ab@gmail.com>
2025-04-12 15:26:50 +0300

    Added token to local storage

--- frontend/src/hooks/useAuthForm.js
+++ frontend/src/hooks/useAuthForm.js
@@ -22,6 +22,8 @@ export const useAuthForm = (type = "login") => {
       const response = type === "login" ? await loginUser(form) : await registerUser(form);
 
       if (response?.success) {
+        localStorage.setItem("token", response.payload);
+        // console.log(response);
         onSuccess?.();
       }
     } catch (err) {
